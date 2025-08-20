bb07abce7abad2944d1c0f5add3c8d4ba5516707
chacha86 <taejincha0619@gmail.com>
2025-08-20 15:36:55 +0900

    work

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Post {
 
     @Id
