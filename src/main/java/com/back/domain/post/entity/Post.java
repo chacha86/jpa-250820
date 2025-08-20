562be82c1d3d9372588094727ca72fd7ee800ff7
chacha86 <taejincha0619@gmail.com>
2025-08-20 15:12:30 +0900

    work

---
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -0,0 +1,18 @@
+package com.back.domain.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+}
