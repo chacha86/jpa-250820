918a4ee5979d8e7c6edeb2ec4923c11021868e47
chacha86 <taejincha0619@gmail.com>
2025-08-26 12:27:34 +0900

    work

--- src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
+++ src/test/java/com/back/domain/post/repository/PostRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,6 +29,8 @@ public class PostRepositoryTest {
 
     @Test
     @DisplayName("글 생성")
+    @Transactional
+    @Rollback
     void t2() {
 
         Post newPost = new Post("new 제목", "new 내용");
@@ -38,7 +42,6 @@ public class PostRepositoryTest {
         assertThat(savedPost.getId()).isNotNull();
         assertThat(savedPost.getTitle()).isEqualTo("new 제목");
         assertThat(savedPost.getContent()).isEqualTo("new 내용");
-
 
     }
 
